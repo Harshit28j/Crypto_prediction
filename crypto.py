c7d558c507972aab59b0cbaaa406843516585a00
Harshit <Harshitjain0562@gmail.com>
2023-02-16 22:42:37 +0530

    added data sets and scripts

---
+++ crypto.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import os
+import time as t
+
+def plot_graph(file_name):
+	data = pd.read_csv(f"{file_name}")
+
+	target = data["Close"]
+	features = data[["Open", "High", "Low", "Volume"]]
+
+	# Split the data into training and testing sets
+	X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2)
+
+	model = LinearRegression()
+
+	# Train the model on the training data
+	model.fit(X_train, y_train)
+
+	# Make predictions on the testing data
+	predictions = model.predict(X_test)
+
+	# Plot the data to visualize the trends
+
+	plt.scatter(data["Open"], target)
+	plt.xlabel("Opening Price")
+	plt.ylabel("Price")
+	plt.title("Opening Price vs Price")
+
+	plt.scatter(data["High"], target)
+	plt.xlabel("Highest Price")
+	plt.ylabel("Price")
+	plt.title("Highest Price vs Price")
+
+	plt.scatter(data["Low"], target)
+	plt.xlabel("Lowest Price")
+	plt.ylabel("Price")
+	plt.title("Lowest Price vs Price")
+
+	plt.scatter(data["Volume"], target)
+	plt.xlabel("Trading Volume")
+	plt.ylabel("Price")
+	plt.title("Trading Volume vs Price")
+
+	# Plotting the difference in market volume vs predicted market volume
+	plt.scatter(X_test["Volume"], predictions - y_test, color='orange', marker='x')
+	plt.xlabel("Trading Volume (Predicted)")
+	plt.ylabel("Price (Predicted) - Price (Actual)")
+	plt.title("Trading Volume (Predicted) vs Price (Predicted) - Price (Actual)")
+	plt.show()
+
+def main():
+	dir_csv=os.listdir()
+	sr_no=1
+	a=0
+	for files in dir_csv:
+		if '.csv' in files:
+			print(f'{sr_no}) {files}')
+			sr_no+=1
+	ask_usr=int(input(">"))
+	if (ask_usr=='') or (ask_usr==None) or (ask_usr>=sr_no):
+		print("invalid selection")
+	else:
+		#to make it look good aise design kr diya h
+		print('You selected dataset:',dir_csv[ask_usr-1])
+		print('Plotting data',end='')
+		while a!=5:
+			print('.'*a,end='')
+			t.sleep(0.2)
+			a+=1
+		
+		plot_graph(dir_csv[ask_usr-1])
+
+if __name__ == '__main__':
+	main()
